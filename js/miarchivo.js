78337c8fdef322dddc6573df83b9cbdab2ab4164
Luis Daniel Torres <luisct91@gmail.com>
2023-01-05 00:11:35 -0300

    arreglos en el carro de compras y en sentencias if

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,13 +1,14 @@
-//DECLARO VARIABLES
-
+//Declaro Variables
 let salirDelMenu = false;
-let cursos = "";
+let cursos;
+let todosLosCursos = "";
+let nombreCurso = "";
 let precioCursos = 0;
-let consulta = "";
-let numeroDeCursos = 0;
-let cursoTotal = 0;
+let consulta;
+let numeroDeCursos;
+let cursoTotal;
 
-//FUNCION PARA MOSTRAR LOS CURSOS DISPONIBLES Y SELECCIONARLOS
+//Funcion para mostrar los cursos
 function mostrarCursos() {
   cursos = parseInt(
     prompt(`
@@ -19,44 +20,66 @@ function mostrarCursos() {
       4 - Programacion BackEnd - Precio del Curso: $6000
       5 - Python - Precio del Curso: $4500
       6 - UX/UI - Precio del Curso: $6000
-      `)
+      
+  Escoge la opcion del 1 al 6 correspondiente al curso al cual 
+  quieras inscribirte.`)
   );
 
+  //Opciones
   switch (cursos) {
     case 1:
-      precioCursos = 3000;
-      cursos = "Desarrollo Web";
-      alert(`Curso ${cursos} agregado al carro de compras`);
+      precioCursos += 3000;
+      nombreCurso = "Desarrollo Web,";
+      alert(`Genial agregaste el curso ${nombreCurso} al carro de compras.
+      Recorda que el inicio de cursada es el dia 15/01/2023`);
+      todosLosCursos += nombreCurso;
       break;
+
     case 2:
-      precioCursos = 3500;
-      cursos = "JavaScript";
-      alert(`Curso de ${cursos} agregado al carro de compras`);
+      precioCursos += 3500;
+      nombreCurso = "JavaScript,";
+      alert(`Genial agregaste el curso ${nombreCurso} al carro de compras
+      Recorda que el inicio de cursada es el dia 20/01/2023`);
+      todosLosCursos += nombreCurso;
       break;
+
     case 3:
-      precioCursos = 3000;
-      cursos = "ReactJS";
-      alert(`Curso de ${cursos} agregado al carro de compras`);
+      precioCursos += 3900;
+      nombreCurso = "ReactJS,";
+      alert(`Genial agregaste el curso ${nombreCurso} al carro de compras
+      Recorda que el inicio de cursada es el dia 05/02/2023`);
+      todosLosCursos += nombreCurso;
       break;
+
     case 4:
-      precioCursos = 3000;
-      cursos = "Programacion BackEnd";
-      alert(`Curso de ${cursos} agregado al carro de compras`);
+      precioCursos += 6000;
+      nombreCurso = "Programacion BackEnd,";
+      alert(`Genial agregaste el curso ${nombreCurso} al carro de compras
+      Recorda que el inicio de cursada es el dia 18/02/2023`);
+      todosLosCursos += nombreCurso;
       break;
+
     case 5:
-      precioCursos = 3000;
-      cursos = "Python";
-      alert(`Curso de ${cursos} agregado al carro de compras`);
+      precioCursos += 4500;
+      nombreCurso = "Python,";
+      alert(
+        `Genial agregaste el curso ${nombreCurso} al carro de compras
+      Recorda que el inicio de cursada es el dia 20/01/2023`
+      );
+      todosLosCursos += nombreCurso;
       break;
+
     case 6:
-      precioCursos = 3000;
-      cursos = "UX/UI";
-      alert(`Curso de ${cursos} agregado al carro de compras`);
+      precioCursos += 6000;
+      nombreCurso = "UX/UI,";
+      alert(`Genial agregaste el curso ${nombreCurso} al carro de compras
+      Recorda que el inicio de cursada es el dia 01/03/2023`);
+      todosLosCursos += nombreCurso;
       break;
+
+    //Opcion Predeterminada
     default:
       alert("Ingrese una opcion del 1 al 6");
-      cursos = "";
-      precioCursos = 0;
       break;
   }
 }
@@ -65,19 +88,26 @@ function mostrarCursos() {
 function carroDeCompras() {
   consulta = prompt(
     `Desea comprar los cursos agregados al carro de compras? Indique si o no.`
-  );
-  if (consulta === "si" || consulta === "SI") {
+  ).toLowerCase();
+
+  if (consulta === "si" /*  || consulta === "SI" */) {
     alert(
-      `¡¡FELICIDADES!! compraste el curso de ${cursos}, preparate para empezar a aprender y disfrutar`
+      `¡¡FELICIDADES!! compraste ${todosLosCursos}, preparate para empezar a aprender y disfrutar`
     );
-  } else {
+    todosLosCursos = "";
+    precioCursos = 0;
+  } else if (consulta == "no") {
     alert(
       "Esperamos que vuelvas pronto y te decidas por algunos de nuestros cursos."
     );
+    todosLosCursos = "";
+    precioCursos = 0;
+  } else {
+    alert("Ingresa si o no");
   }
 }
 
-//DO WHILE PARA EL MENU
+//Menu Principal
 do {
   let consultar = parseInt(
     prompt(`Ingrese la opcion deseada: 
@@ -88,10 +118,13 @@ do {
     0 - Salir del menu.
     `)
   );
+
+  //Opciones
   switch (consultar) {
     case 1:
       mostrarCursos();
       break;
+
     case 2:
       alert(`A continuacion te detallamos el inicio por cada curso que tenemos disponible:
 
@@ -104,16 +137,26 @@ do {
       
       Escoge el que mas te convenga!.`);
       break;
+
     case 3:
-      alert(
-        `En el carro de compras agregaste ${cursos} y el precio es $${precioCursos}.`
-      );
-      carroDeCompras();
+      if (todosLosCursos == "") {
+        alert("El carro esta vacio, agrega los cursos que mas te gusten!.");
+      } else {
+        alert(
+          `En el carro de compras agregaste ${todosLosCursos} y el precio total es de: $${precioCursos}.`
+        );
+        carroDeCompras();
+      }
       break;
+
     case 0:
-      alert("GRACIAS POR VISITARNOS, ESPERAMOS QUE PRONTO TENGAMOS ALGUN CURSO QUE TE ANIME A CAPACITARTE CON NOSOTROS. ");
+      alert(
+        "GRACIAS POR VISITARNOS, ESPERAMOS QUE PRONTO TENGAMOS ALGUN CURSO QUE TE ANIME A CAPACITARTE CON NOSOTROS. "
+      );
       salirDelMenu = true;
       break;
+
+    //Opcion Predeterminada
     default:
       alert("Ingrese una opcion correcta del 0 al 3.");
       break;
